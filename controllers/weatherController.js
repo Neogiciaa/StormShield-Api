f3b53cc0be5344e4d6db3c65270a7ff7c482c3d0
Pedrocito97 <pierre.willemart.110497@gmail.com>
2024-12-03 11:19:38 +0100

    updated weatherController with object structure

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -3,7 +3,20 @@ import {fetchWeatherData} from '../models/weatherModel.js';
 export const getWeatherDatas = async (req, res) => {
     try {
         const weatherData = await fetchWeatherData();
-        res.status(200).json(weatherData);
+        const weatherDataFormatted = weatherData.list.filter(data => data.dt_txt.includes('12:00:00')).map(data => {
+            return {
+                date: data.dt_txt,
+                temperature: data.main.temp,
+                humidity: data.main.humidity,
+                cloud: data.clouds.all,
+                wind: data.wind.speed,
+                pressure: data.main.pressure,
+                snow: data.snow,
+                rain: data.rain,
+            }
+        });
+        res.status(200).json(weatherDataFormatted);
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
