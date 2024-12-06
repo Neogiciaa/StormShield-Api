2d073cb45d0078ce97a8db49725b9727fd74e507
Pedrocito97 <pierre.willemart.110497@gmail.com>
2024-12-06 11:37:41 +0100

    Save commit

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -14,11 +14,11 @@ const alerts = {
     },
     'storm': {
         'moderate': {
-            'wind': { min: (51/3.6), max: (80/3.6)}, // Strong wind (orange)
+            'wind': { min: (51), max: (80)}, // Strong wind (orange)
             'pressure': { max: 1014, min: 1005 } // Low pressure (orange)
         },
         'high': {
-            'wind': { min: (81/3.6) }, // Very strong wind (red or extreme-red)
+            'wind': { min: (81) }, // Very strong wind (red or extreme-red)
             'pressure': { max: 1004 } // Very low pressure (red or extreme-red)
         }
     },
@@ -34,10 +34,10 @@ const alerts = {
     },
     'strong-wind': {
         'moderate': {
-            'wind': { min: (51/3.6), max: (80/3.6) } // Strong wind (orange)
+            'wind': { min: (51), max: (80) } // Strong wind (orange)
         },
         'high': {
-            'wind': { min: (81/3.6) } // Very strong wind (red or extreme-red)
+            'wind': { min: (81) } // Very strong wind (red or extreme-red)
         }
     },
     'extreme-heat': {
@@ -53,11 +53,11 @@ const alerts = {
     'extreme-cold': {
         'moderate': {
             'temperature': { max: -11, min: -24 }, // Very cold (light-blue)
-            'wind': { min: 0, max: (80/3.6) } // Strong wind (orange)
+            'wind': { min: 0, max: (80) } // Strong wind (orange)
         },
         'high': {
             'temperature': { max: -25, min: -100 }, // Extreme cold (blue)
-            'wind': { min: 0, max: (80/3.6) } // Very strong wind (red or extreme-red)
+            'wind': { min: 0, max: (80) } // Very strong wind (red or extreme-red)
         }
     }
 };
@@ -65,14 +65,14 @@ const alerts = {
 async function checkAlerts(weatherData, lat, lon) {
     if (weatherData.rain >= alerts.flood.moderate.rain.min && weatherData.rain <= alerts.flood.moderate.rain.max && weatherData.pressure <= alerts.flood.moderate.pressure.max && weatherData.pressure >= alerts.flood.moderate.pressure.min) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'Moderate Flood Alert', weatherData.rain, weatherData.pressure);
+        await createAlert(newLocationId, 'Moderate Flood Alert', weatherData.rain +'mm', weatherData.pressure + "hPa");
     }
     if (
         weatherData.rain >= alerts.flood.high.rain.min &&
         weatherData.pressure <= alerts.flood.high.pressure.max
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'High Flood Alert', weatherData.rain, weatherData.pressure);
+        await createAlert(newLocationId, 'High Flood Alert', weatherData.rain + 'mm', weatherData.pressure + "hPa");
     }
 
     // Storm alert
@@ -84,14 +84,14 @@ async function checkAlerts(weatherData, lat, lon) {
     ) {
 
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'Moderate Storm Alert', weatherData.wind, weatherData.pressure);
+        await createAlert(newLocationId, 'Moderate Storm Alert', weatherData.wind + " km/h", weatherData.pressure + "hPa");
     }
     if (
         weatherData.wind >= alerts.storm.high.wind.min &&
         weatherData.pressure <= alerts.storm.high.pressure.max
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'High Storm Alert', weatherData.wind, weatherData.pressure);
+        await createAlert(newLocationId, 'High Storm Alert', weatherData.wind+ " km/h", weatherData.pressure + "hPa");
     }
 
     // Ice risk alert
@@ -102,14 +102,14 @@ async function checkAlerts(weatherData, lat, lon) {
         weatherData.rain <= alerts['ice-risk'].moderate.rain.max)
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'Moderate Ice Risk Alert', weatherData.temperature, weatherData.rain);
+        await createAlert(newLocationId, 'Moderate Ice Risk Alert', weatherData.temperature +"°C", weatherData.rain +"mm");
     }
     if (
         weatherData.temperature <= alerts['ice-risk'].high.temperature.max &&
         weatherData.rain >= alerts['ice-risk'].high.rain.min
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'High Ice Risk Alert', weatherData.temperature, weatherData.rain);
+        await createAlert(newLocationId, 'High Ice Risk Alert', weatherData.temperature + "°C", weatherData.rain + "mm");
     }
 
     // Strong wind alert
@@ -122,7 +122,7 @@ async function checkAlerts(weatherData, lat, lon) {
     }
     if (weatherData.wind >= alerts['strong-wind'].high.wind.min) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'High Strong Wind Alert', weatherData.wind);
+        await createAlert(newLocationId, 'High Strong Wind Alert', weatherData.wind + " km/h");
     }
 
     // Extreme heat alert
@@ -133,14 +133,14 @@ async function checkAlerts(weatherData, lat, lon) {
         weatherData.pressure <= alerts['extreme-heat'].moderate.pressure.max)
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'Moderate Extreme Heat Alert', weatherData.temperature, weatherData.pressure);
+        await createAlert(newLocationId, 'Moderate Extreme Heat Alert', weatherData.temperature + "°C", weatherData.pressure + "hPa");
     }
     if (
         weatherData.temperature >= alerts['extreme-heat'].high.temperature.min &&
         weatherData.pressure >= alerts['extreme-heat'].high.pressure.min
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'High Extreme Heat Alert', weatherData.temperature, weatherData.pressure);
+        await createAlert(newLocationId, 'High Extreme Heat Alert', weatherData.temperature + "°C", weatherData.pressure + "hPa");
     }
 
     // Extreme cold alert
@@ -151,20 +151,20 @@ async function checkAlerts(weatherData, lat, lon) {
         weatherData.wind <= alerts['extreme-cold'].moderate.wind.max)
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'Moderate Extreme Cold Alert', weatherData.temperature, weatherData.wind);
+        await createAlert(newLocationId, 'Moderate Extreme Cold Alert', weatherData.temperature + "°C", weatherData.wind + " km/h");
     }
     if (
         weatherData.temperature <= alerts['extreme-cold'].high.temperature.max &&
         weatherData.wind >= alerts['extreme-cold'].high.wind.min
     ) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert(newLocationId, 'High Extreme Cold Alert', weatherData.temperature, weatherData.wind);
+        await createAlert(newLocationId, 'High Extreme Cold Alert', weatherData.temperature +"°C", weatherData.wind + " km/h");
     }
 
     // TODO A delete quand tout sera good !
     if (weatherData.temperature === 2.15) {
         const newLocationId = await saveLocationAndGetLocationId(lat, lon);
-        await createAlert (newLocationId, 'Test Alerte', weatherData.pressure);
+        await createAlert (newLocationId, 'Test Alerte', weatherData.pressure + "hPa");
     }
 }
 
@@ -178,7 +178,7 @@ export const getWeatherDatas = async (req, res) => {
             temperature: data.main.temp,
             humidity: data.main.humidity,
             cloud: data.clouds.all,
-            wind: data.wind.speed,
+            wind: (data.wind.speed * 3.6).toFixed(1),
             pressure: data.main.pressure,
             snow: data.snow || 0,
             rain: data.rain?.['3h'] || 0,
@@ -205,7 +205,7 @@ export const getWeatherDatasByCityName = async (req, res) => {
             temperature: data.main.temp,
             humidity: data.main.humidity,
             cloud: data.clouds.all,
-            wind: data.wind.speed,
+            wind: (data.wind.speed * 3.6).toFixed(1),
             pressure: data.main.pressure,
             snow: data.snow || 0,
             rain: data.rain?.['3h'] || 0,
